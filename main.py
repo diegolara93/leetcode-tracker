06c3d784c3b6618650fa9a190b651c8b6e017b15
Diego <diegolara93345@gmail.com>
2023-06-26 22:42:03 -0700

    added more prompts

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import os
 import requests
 import json
 os.system('color')
+
 response = requests.get("https://leetcode-stats-api.herokuapp.com/diegolara93345")
-text = colored("Hello, World!", "red", attrs=["reverse", "blink"])
-print(text)
-print(response.json()["totalSolved"])
+print(response.json())
+print("Your current total solved leetcode problems are:",colored(response.json()["totalSolved"], "red"))
+print("Your current total easy solved leetcode problems are:",colored(response.json()["easySolved"], "red"))
+print("Your current total medium solved leetcode problems are:",colored(response.json()["mediumSolved"], "red"))
+print("Your current total hard solved leetcode problems are:",colored(response.json()["hardSolved"], "red"))
+print("Your current ranking is",colored(response.json()["ranking"], "red"))
 n = 1
 while n == 1:
     h = 3
