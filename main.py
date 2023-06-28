1597402a3f35a28f2408dbec3a027c8c3b4f4443
Diego <diegolara93345@gmail.com>
2023-06-27 23:03:41 -0700

    added incorrect input prompt

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-from termcolor import colored, cprint
+from termcolor import colored
 import os
 import requests
-import json
-os.system('color')
 
-response = requests.get("https://leetcode-stats-api.herokuapp.com/diegolara93345")
-print(response.json())
-print("Your current total solved leetcode problems are:",colored(response.json()["totalSolved"], "red"))
-print("Your current total easy solved leetcode problems are:",colored(response.json()["easySolved"], "red"))
-print("Your current total medium solved leetcode problems are:",colored(response.json()["mediumSolved"], "red"))
-print("Your current total hard solved leetcode problems are:",colored(response.json()["hardSolved"], "red"))
-print("Your current ranking is",colored(response.json()["ranking"], "red"))
-n = 1
-while n == 1:
-    h = 3
+os.system('color')
+print(colored("Welcome", "blue", attrs=["bold","dark"]))
+print("Enter your leetcode username")
+username = input()
+try:
+    response = requests.get(f"https://leetcode-stats-api.herokuapp.com/{username}")
+    print("Your current total solved leetcode problems are:",colored(response.json()["totalSolved"], "cyan", attrs=["bold","dark"]))
+    print("Your current total easy solved leetcode problems are:",colored(response.json()["easySolved"], "cyan", attrs=["bold","dark"]))
+    print("Your current total medium solved leetcode problems are:",colored(response.json()["mediumSolved"], "cyan", attrs=["bold","dark"]))
+    print("Your current total hard solved leetcode problems are:",colored(response.json()["hardSolved"], "cyan", attrs=["bold","dark"]))
+    print("Your current ranking is",colored(response.json()["ranking"], "cyan"))
+except:
+    print(colored("ERROR INCORRECT USERNAME", "red", attrs=["bold","dark","underline"]))
+# print(response.json())  
+
+while True: 
+    k=1
